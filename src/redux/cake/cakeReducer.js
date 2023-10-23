e28ab12d90f7c69b573c8c6840164a5d1fcfd1b1
behzad <90694618+behzadiv@users.noreply.github.com>
2023-10-23 11:59:04 +0330

    feat : create cakeTypes

--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,3 +1,4 @@
+import { BUY_CAKE } from "./cakeTypes";
 
 const initialState = {
   numOfCake: 10,
@@ -5,7 +6,7 @@ const initialState = {
 
 export default function CakeReducer(state = initialState, action) {
   switch (action.type) {
-    case "BUY_CAKE":
+    case BUY_CAKE:
       return {
         ...state,
         numOfCake: state.numOfCake - action.payload,
