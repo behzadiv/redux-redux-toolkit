937c5944913d91877f23f3ae8cc7172711ffdf3b
behzad <90694618+behzadiv@users.noreply.github.com>
2023-10-23 09:57:04 +0330

    feat : create CakeReducer and store

---
+++ src/redux/cake/CakeReducer.jsx
@@ -0,0 +1,15 @@
+const initialState = {
+  numOfCake: 10,
+};
+
+export default function CakeReducer(state = initialState, action) {
+  switch (action.type) {
+    case "BUY_CAKE":
+      return {
+        ...state,
+        numOfCake: state.numOfCake - 1,
+      };
+    default:
+      return state;
+  }
+}
