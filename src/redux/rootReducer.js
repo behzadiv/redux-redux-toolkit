f9202fc56d3701e9f5e6fb4f737237947573df2e
behzad <90694618+behzadiv@users.noreply.github.com>
2023-10-24 10:55:00 +0330

    feat : add userReducer to rootReducer and store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import CakeReducer from "./cake/cakeReducer";
 import MilkReducer from "./milk/milkReducer";
-
+import UserReducer from "./user/userReducer";
 
 export const rootReducer = combineReducers({
   cake: CakeReducer,
   milk: MilkReducer,
+  user: UserReducer,
 });
