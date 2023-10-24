325896a4b3d9c80f9060c18e259376a8f38c8a73
behzad <90694618+behzadiv@users.noreply.github.com>
2023-10-24 10:55:27 +0330

    feat : create user component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import MilkContainer from "./components/MilkContainer";
+import User from "./components/User";
 import store from "./redux/store";
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <Provider store={store}>
       <CakeContainer />
       <MilkContainer />
+      <hr />
+      <User />
     </Provider>
   );
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import fetchUsers from "../redux/user/userActions";
+
+const User = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user);
+  const getUsers = () => {
+    dispatch(fetchUsers());
+  };
+
+  return (
+    <div>
+      <button onClick={() => getUsers()}>Fetch Users</button>
+      <div>
+        {users.loading ? (
+          <p>loading...</p>
+        ) : users.error ? (
+          <p>{users.error}</p>
+        ) : (
+          <div>
+            {users.data.map((item) => {
+              return <h5>{item.name}</h5>;
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default User;
